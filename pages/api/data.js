2b9e1d78c6969914efdfe9d2c91e3fb77fe1141a
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2023-02-05 21:17:13 +0530

    REST API implemented

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,47 +1,65 @@
 import { ElevatorSharp, TrySharp } from "@mui/icons-material";
-import {app, database} from '../../firebase-config';
-import {addDoc, collection} from 'firebase/firestore';
+import { app, database } from "../../firebase-config";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
-export default function handler(req,res){
-    
-   const  feedbackCollectionRef = collection(database,'feedback');
-    if(req.method == 'POST'){
-        console.log("Hi from API");
-        const data = JSON.parse(req.body)
-        try{
-            addDoc(feedbackCollectionRef, {
-                email:data.email,
-                text : data.text
-            });
-            res.status(200).json({
-                message : 'SUCCESS'
-            })
-           
-        }
-        catch(err){
-             res.status(500).json({
-                message : 'failed'
-             })
-        }
+export default function handler(req, res) {
+  const feedbackCollectionRef = collection(database, "feedback");
+  if (req.method == "POST") {
+    console.log("Hi from API");
+    const data = JSON.parse(req.body);
+    try {
+      addDoc(feedbackCollectionRef, {
+        email: data.email,
+        text: data.text,
+      });
+      res.status(200).json({
+        message: "SUCCESS",
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "failed",
+      });
     }
-    const db = firebase.firestore();
+  } else if (req.method == "GET") {
+    let documents = [];
+    try {
+      getDocs(feedbackCollectionRef)
+        .then((data) => {
+          data.docs.forEach((doc) => {
+            let document = {
+              email: doc.data().email,
+              text: doc.data().text,
+            };
+            documents.push(document);
+          });
+        })
+        .then(() => {
+          res.status(200).json({
+            message: "SUCCESS",
+            data: documents,
+          });
+        });
+    } catch (err) {
+      res.status(500).json({
+        message: "failed",
+      });
+    }
+  }
 
-// db.collection('feedback')
-//   .get()
-//   .then(snapshot => {
-//     let data = [];
-//     snapshot.forEach(doc => {
-//       let document = {
-//         email: doc.data().email,
-//         text: doc.data().text
-//       };
-//       data.push(document);
-//     });
-//     return res.status(200).json(data);
-//   })
-//   .catch(error => {
-//     return res.status(500).json({ message: 'Error fetching data: ' + error });
-//   });
-
-    
+  // db.collection('feedback')
+  //   .get()
+  //   .then(snapshot => {
+  //     let data = [];
+  //     snapshot.forEach(doc => {
+  //       let document = {
+  //         email: doc.data().email,
+  //         text: doc.data().text
+  //       };
+  //       data.push(document);
+  //     });
+  //     return res.status(200).json(data);
+  //   })
+  //   .catch(error => {
+  //     return res.status(500).json({ message: 'Error fetching data: ' + error });
+  //   });
 }
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,11 +1,80 @@
-const FetchStats = () =>{
-    return(
-        <div>
-            This is fetch stats page;
-        </div>
-    )
-}
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CircularProgress from "@mui/material/CircularProgress";
+import { TableContainer } from "@mui/material";
 
+const FetchStats = () => {
+  const [documents, setDocuments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/data", {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response.message);
+        setDocuments(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div style={{ width: "80%", margin: "0 auto", marginTop: "40%" }}>
+        Loading...
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>An error occurred: {error}</div>;
+  }
+
+  return (
+    <div>
+      <Card
+        sx={{
+          width: "80%",
+          margin: "0 auto",
+          marginTop: "2rem",
+          padding: "5rem",
+        }}
+      >
+        <TableContainer>
+          <TableHead>
+            <TableRow>
+              <TableCell>Email</TableCell>
+              <TableCell align="right">Feedback</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {documents.map((item: any) => (
+              <TableRow key={item.email}>
+                <TableCell>{item.email}</TableCell>
+                <TableCell align="right">{item.text}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TableContainer>
+      </Card>
+    </div>
+  );
+};
 
 export default FetchStats;
